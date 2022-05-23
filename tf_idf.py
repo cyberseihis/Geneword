0f51bec6c3730f71381fcc65b98fa71df05d5f6c
cyberseihis <mcraftdm@gmail.com>
2022-05-23 21:09:21 +0300

    Refactored diag matrices with row multiplication

--- tf_idf.py
+++ tf_idf.py
@@ -6,15 +6,14 @@ def tf_idf(bags: np.ndarray) -> np.ndarray:
     # get array of total words per individual text
     totalText = np.sum(bags, axis=1)
     totalText = 1/totalText
-    # turn to matrix for multiplication with bag of word matrix
-    totalTxt = np.diagflat(totalText)
-    tf = totalTxt @ bags
+    totalText = np.vstack(totalText)
+    tf = totalText * bags
     # number of texts each word appears in
     appears = np.count_nonzero(bags, axis=0)
     N = np.size(bags, axis=0)
     idf = np.log2(N) - np.log2(appears)
-    tfidf = tf @ np.diagflat(idf)
-    return tfidf
+    tfidf = tf * idf
+    return tfidf, totalText, idf
 
 
 if __name__ == '__main__':
