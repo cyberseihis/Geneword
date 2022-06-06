790994b455dca1fff5b1f42bba4690c9cd1be059
cyberseihis <mcraftdm@gmail.com>
2022-06-07 01:13:28 +0300

    Fixed fitness, refactored survive, changed max punish

--- Jean.py
+++ Jean.py
@@ -16,7 +16,8 @@ def init_population(size: int, P: float = 0.5) -> narr:
 
 def fitness_score(pop: narr, weights: narr, maxPunish: float) -> narr:
     # get average tfidf score of words in each member
-    wordScore = np.mean(pop * weights, axis=1)
+    word_count = np.count_nonzero(pop, axis=1)
+    wordScore = np.sum(pop * weights, axis=1) / word_count
     # calculate punishment for ammount of words over minimum
     wordCount = np.sum(pop, axis=1) - 1000
     punishment = wordCount / (8520 - 1000) * maxPunish
@@ -24,15 +25,19 @@ def fitness_score(pop: narr, weights: narr, maxPunish: float) -> narr:
 
 
 # roulete based on score
-def survivor_index(scores: narr) -> narr:
+def roulete(scores: narr) -> narr:
     probs = scores / np.sum(scores)
+    return probs
+
+
+def survive(pop: narr,
+            evaluator: Callable[[narr], narr],
+            score2prob: Callable[[narr], narr]) -> narr:
+    scores = evaluator(pop)
+    probs = score2prob(scores)
     popN = np.size(scores)
-    survivors = rng.choice(popN, popN, p=probs)
-    return survivors
-
-
-def survivor_population(pop: narr, idx: narr) -> narr:
-    return pop[idx]
+    survivor_idx = rng.choice(popN, popN, p=probs)
+    return pop[survivor_idx], scores
 
 
 # Given a population select some of them for breeding and return
@@ -56,14 +61,13 @@ def mutate(pop: narr, P: float = 0.01) -> narr:
 # Given a population run the genetic algorithm for one generation
 # and return the next population
 def next_generation(pop: narr,
-                    evaluator: Callable[[narr], narr],
+                    survivor: Callable[[narr], narr],
                     breeder: Callable[[narr], narr],
                     mutator: Callable[[narr], narr],
                     ) -> tuple[narr, narr]:
-    scores = evaluator(pop)
-    survivors = survivor_population(pop, survivor_index(scores))
-    hatchlings = breeder(survivors)
-    children = mutator(hatchlings)
+    survivors, scores = survivor(pop)
+    babies = breeder(survivors)
+    children = mutator(babies)
     return children, scores
 
 
@@ -79,23 +83,34 @@ def train(pop_size: int, p_breed: float, p_mut: float,
     old_scores = scores
     gen_count = 0
     max_gens = 1000
+    survivor = partial(survive, evaluator=evaluator, score2prob=roulete)
     breeder = partial(breed_survivors, P=p_breed)
     mutator = partial(mutate, P=p_mut)
     next_gen = partial(next_generation,
                        breeder=breeder, mutator=mutator,
-                       evaluator=evaluator)
+                       survivor=survivor)
     while (scores >= old_scores and gen_count < max_gens):
         gen_count += 1
         old_scores = scores
         pop, scores = next_gen(pop)
-        history.append(scores)
+        history.append(np.mean(scores))
     return pop, history
 
 
+# MAYBE CHANGE TO ONLY RETURNING BEST POPULATION
+def batch_train(trainer: Callable[[], tuple[narr, list[float]]]):
+    return (trainer() for _ in range(10))
+
+
 def main():
+    config_table = [[20, 0.6, 0], [20, 0.6, 0.01]]
     # Created by save_weights of tf_idf.py
     weights = np.load("tfidf_weights.npz")['a']
-    # max punish set to the maximul score average of tf_idfs
-    # can give to 1000 words
+    # max punish set to the tf_idf score of a member with
+    # all words selected, such that a member
+    # with all words is scored 0
     evaluator = partial(fitness_score, weights=weights,
-                        maxPunish=0.002667750770568537)
+                        maxPunish=np.mean(weights))
+    trainer = partial(train, evaluator=evaluator)
+    # MISSING TENTRAINS
+    my_trainers = (partial(trainer, x, y, z) for x, y, z in config_table)
