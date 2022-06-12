90aa3ad35a878bb683291f014fa0c46280d97871
cyberseihis <mcraftdm@gmail.com>
2022-06-13 00:33:42 +0300

    Added graphs of performances

---
+++ Graphs.py
@@ -0,0 +1,26 @@
+import holoviews as hv
+import pickle
+
+
+def fixup(hist):
+    return [(i, max(h, 0)) for i, h in enumerate(hist)]
+
+
+def meta2graph(meta):
+    curve_dict = {i: hv.Curve(fixup(hist), "generation", "score")
+                  for i, hist in enumerate(meta)}
+    return hv.NdOverlay(curve_dict, kdims="run")
+
+
+def get_graphs():
+    hv.extension('matplotlib')
+    with open("genetics.pkl", 'rb') as fi:
+        ch, complete_history = pickle.load(fi)
+    meta_dict = {i: meta2graph(met) for i, met in complete_history.items()}
+    big_graph = hv.HoloMap(meta_dict,
+                           kdims=["Pop. size", "Pbreed", "Pmutate"])
+    hv.save(big_graph.layout(), 'big_graph.svg')
+
+
+if __name__ == '__main__':
+    get_graphs()
